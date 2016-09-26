61757a1538aee3f2040db95c106fe647692e6c00
DVPitakov <DVPitakov@gmail.com>
2016-09-26 19:26:04 +0300

    pitackov second commit

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
 module.exports = {
 	"swagger": "2.0",
 	"info": {
-		"version": "0.0.3",
-		"title": "TechnoChat API",
-		"description": "**ТехноЧат**"
+		"version": "0.0.1",
+		"title": "API игры Монополия",
+		"description": "**Монополия онлайн**"
 	},
 	"basePath": "/api",
-	"schemes": ["http"],
+	"schemes": ["http","ws"],
    	"host": "http://localhost:3000",
 
 	paths: {
-		'/messages': require('./resources/messages')
+		'/signup': require('./resources/signup'),
+		'/signin': require('./resources/signin'),
+		'/signout': require('./resources/signout'),
+		'/game': require('./resources/game')
 	},
 
 	definitions: {
-		Message: require('./scheme/Message'),
+		Game: require('./scheme/Game'),
+		WSMessage: require('./scheme/WSMessage'),
 	}
 
 }
---
+++ api/resources/signup.js
@@ -0,0 +1,64 @@
+exports.post = {
+	"tags": ["signup"],
+	"description": "Метод регистрирует нового пользователя",
+	"parameters": [
+		{
+			"name": "email",
+			"description": "Электроная почта пользователя",
+			"type": "string"
+		},
+		{
+			"name": "name",
+			"description": "Имя пользователя",
+			"type": "string"
+		},
+		{
+			"name": "password",
+			"description": "Пароль пользователя",
+			"type": "string"
+		},
+
+	],
+	"responses": {
+		"200": {
+			"schema": {
+				"description": "Содержит ключь сессии пользователя",
+				"type": "string"
+			}
+		},
+		"400": {
+			"description": "Ошибка при выполнении запроса"
+		}
+	},
+	"x-amples": [{
+		"description": "создание тестового сообщения",
+		"request": {
+			"params": {
+				"login": "test",
+				"text": "Проверяем"
+			}
+		},
+		"response": {
+			"status": 200,
+			"headers": {
+				"content-type": "application/json"
+			},
+			"validator": function (res) {
+
+				if (typeof res.id !== 'number' ) {
+					return 'не корректный id';
+				}
+
+				if (typeof res.message !== 'string') {
+					return 'не корректный текст сообщения';
+				}
+
+				if (typeof res.email !== 'string') {
+					return 'не корректный login';
+				}
+
+				return true;
+			}
+		}
+	}]
+};
