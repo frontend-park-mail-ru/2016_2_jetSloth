9bda7b3a5f48b0b74f69ff9246f0d9679f2c6d65
Kirnic <Kirujke@MacBook-Air-Kirill.local>
2016-09-30 18:23:31 +0300

    добавлен дизайн

--- public/components/input/input.js
+++ public/components/input/input.js
@@ -1,40 +1,40 @@
-(function () {
+(function() {
     'use strict';
 
     class Input {
-        constructor (type, name, title) {
-	    this.body = document.createElement('div');
+        constructor(type, name, title) {
+            this.body = document.createElement('div');
 
-	    this.title = document.createElement('div');
- 	    this.className = "startInput";
-	    this.title.innerHTML = title;
+            this.title = document.createElement('div');
+            this.className = "startInput";
+            this.title.innerHTML = title;
 
-	    this.title = document.createElement('div');
- 	    this.className = "endInput";
-	    this.title.innerHTML = title;
+            this.title = document.createElement('div');
+            this.className = "endInput";
+            this.title.innerHTML = title;
 
-	    this.input = document.createElement('input');
-	    this.input.setAttribute("type", type);
-	    this.input.setAttribute("name", name);
-	    this.input.className = 'validInput';
-         
-	    this.body.appendChild(this.title);
-	    this.body.appendChild(this.input);
-		
-	    return this.body;
-   
+            this.input = document.createElement('input');
+            this.input.setAttribute("type", type);
+            this.input.setAttribute("name", name);
+            // this.input.className = 'validInput';
+
+            this.body.appendChild(this.title);
+            this.body.appendChild(this.input);
+
+            return this.body;
+
         }
-	valid (bool) {
-		this.input.className = bool?'validInput':'invalidInput';
-	}
+        valid(bool) {
+            this.input.className = bool ? 'validInput' : 'invalidInput';
+        }
     }
 
     //export
-	if (typeof exports === 'object') { // for NodeJS
-		exports.Input = Input;
-	} else {
-		 window.Input = Input;
-	}
+    if (typeof exports === 'object') { // for NodeJS
+        exports.Input = Input;
+    } else {
+        window.Input = Input;
+    }
 
 
 })();
