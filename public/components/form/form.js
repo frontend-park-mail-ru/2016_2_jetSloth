a4429060c9fe0ed604e752f4047c89bbc2725ea2
Kirnic <nickirill@mail.ru>
2016-11-14 17:01:20 +0300

    base index/signin/signup pages works

--- public/components/form/form.js
+++ public/components/form/form.js
@@ -1,49 +1,33 @@
 (function () {
     'use strict';
 
-    // import
     const Block = window.Block;
     const Button = window.Button;
     const Input = window.Input;
 
     class Form extends Block {
-
-        /**
-         * Конструктор класса Form
-         */
         constructor(options = {data: {}}) {
             super('form');
-            // this.template = window.fest['form/form.tmpl'];
+            this.template = window.fest['form/form.tmpl'];
             this.data = options.data;
             this._el = options.el;
             this.render();
         }
 
-        /**
-         * Обновляем HTML
-         */
         render() {
-            // this._updateHtml();
+            this._updateHtml();
             this._installItems();
         }
 
-        /**
-         * Обнуляем форму
-         */
         reset() {
             this._el.querySelector('form').reset();
         }
 
-        /**
-         * Обновить html компонента
-         */
         _updateHtml() {
-            // this._el.innerHTML = this.template(this.data);
+            this._el.innerHTML = this.template(this.data);
         }
 
-        /**
-         * Вставить управляющие элементы в форму
-         */
+
         _installItems() {
             let {fields = []} = this.data;
 
@@ -55,7 +39,8 @@
                         placeholder: data.placeholder
                     }
                 });
-                this._el.appendChild(input._get());
+                this._el.querySelector('.js-inputs').appendChild(input._get());
+
             });
 
 
@@ -66,7 +51,7 @@
                     text: data.text,
                     attrs: data.attrs
                     });
-                this._el.appendChild(control._get());
+                this._el.querySelector('.js-controls').appendChild(control._get());
             });
         }
     }
